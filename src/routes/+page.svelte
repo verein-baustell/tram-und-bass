f228d237ba2a9053a282ce69ac7743411b2f1000
dariush-m-ta <dariush.mehdiaraghi@outlook.com>
2024-05-19 18:29:33 +0200

    feat: video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,50 @@
 <script lang="ts">
   import { attributes as content } from "../content/lines.md";
   import SvelteMarkdown from "svelte-markdown";
-  // import the component
   import Button from "../lib/Button.svelte";
+  import Vimeo from "@vimeo/player";
+  import { onMount } from "svelte";
+
   const lines = content.lines;
+  const currentLine = lines[0];
+  let player: Vimeo;
+  onMount(() => {
+    player = new Vimeo("video-container", {
+      url: currentLine.videoUrl,
+      controls: false,
+    });
+    player.ready().then(() => {
+      const iframe = document.querySelector("#video-container iframe");
+      if (iframe) {
+        iframe.style.minWidth = "100%";
+        iframe.style.minHeight = "100%";
+      }
+    });
+  });
+  const playVideo = () => {
+    console.log("play video");
+    player.play();
+  };
 </script>
 
 <h1>Welcome to TnB</h1>
 {#each lines as line}
   <p>artist: {line.artistName}</p>
   <p>name: {line.name}</p>
+  <div id="video-container"></div>
   <SvelteMarkdown source={line.artistAboutText} />
-  {#each line.timeStamps as timeStamp}
-    <p>startTime: {timeStamp.startTime}</p>
-    <p>endTime: {timeStamp.endTime}</p>
-    <p>location: {timeStamp.name}</p>
-  {/each}
-  <!-- insert the component and declare what the prop is -->
-    <Button label={line.name} />
-  {/each}
+  <Button on:click={() => playVideo()}>Play</Button>
+{/each}
+
+<style lang="scss">
+  #video-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: grid;
+    place-items: center;
+    z-index: -1;
+  }
+</style>
