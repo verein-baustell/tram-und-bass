4f872f9e5cf20800c9957f9a472efba71ee68b2a
Fabian Frey <50176941+fabidesign@users.noreply.github.com>
2024-07-19 11:01:00 +0200

    logos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,7 +62,17 @@
   style={`width: ${videoWrapperWidth}; height: ${videoWrapperHeight};`}
 ></div>
 <div class="blur-vignette"></div>
-<div class="logo"><img src="/images/tram_bass_2-15.png" width="auto" height="auto"></div>
+<div class="splash">
+  <img class="logo" src="/images/tram_bass_2-15.png" width="auto" height="auto">
+  <div class="supporters">
+    <img class="supporter" src="/images/Stadt.png">
+    <img class="supporter" src="/images/Migros.png">
+    <img class="supporter" src="/images/ErnstGohner.png">
+    <img class="supporter" src="/images/Temperatio.png">
+    <img class="supporter" src="/images/Kanton.png">
+    <img class="supporter" src="/images/VBZ.png">
+  </div>
+</div>
 <VideoControls />
 {#if isDevMode}
   <DevTools />{/if}
@@ -127,6 +137,40 @@
     z-index: -1;
   }
 
+  .splash{
+		z-index: 1000;
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+	}
+
+  .logo{
+		width: auto;
+		height: 300px;
+    margin: auto;
+	}
+
+  .supporters{
+    display: flex;
+    justify-content : space-between;
+    margin-left: auto;
+    margin-right: auto;
+    width: calc(100vw - 1.6em);
+    padding: 1em;
+    align-items: center;
+    border-radius: var(--border-radius-view);
+    background-color: var(--background-color);
+  }
+
+  .supporter{
+    height: 30px;
+    width: auto;
+    
+  }
 
   .blur-vignette {
   --radius: 0px;
