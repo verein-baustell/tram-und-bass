b875cd1521a6def3c7cef7cdf87f224d8d5ff461
Fabian Frey <50176941+fabidesign@users.noreply.github.com>
2024-09-06 18:02:05 +0200

    prod

--- src/lib/LandingScreen.svelte
+++ src/lib/LandingScreen.svelte
@@ -1,13 +1,46 @@
 <div class="splash">
-  <button>
-    hi
-  </button>
-  <div class="newsletter">
-    <form method="post" action="https://newsletter.infomaniak.com/v3/api/1/newsletters/webforms/18147/submit" class="inf-form"><input type="email" name="email" style="display:none" /><input type="hidden" name="key" value="eyJpdiI6Ik1qdEt3VGk1WXJQSzc1MkFKYkN3NExWT01WZ05TTTc4bkVRVEFiSHNaa3M9IiwibWFjIjoiZTE3OTZmMGFjYzJhMmEyODFhYmJmZmNhMThhMTExYjIxZGJkZWY3MDQ4ZDkxMTQ0MTFmMzk5YzJjYmI1OWM1MyIsInZhbHVlIjoibmxJY1h0eG9Gd3pBWGxDTG1ldzFSaDFUUTlua2NERWFJbHoxQUtPSW00bz0ifQ=="><input type="hidden" name="webform_id" value="18147"><style> .inf-main_41371be284c70c592049717c38e42081{ background-color:#f5f5f5; padding:25px 20px; margin:25px auto; } .inf-main_41371be284c70c592049717c38e42081 .inf-content { margin-top:13px;} .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 span, .inf-main_41371be284c70c592049717c38e42081 label, .inf-main_41371be284c70c592049717c38e42081 input, .inf-main_41371be284c70c592049717c38e42081 .inf-submit, .inf-main_41371be284c70c592049717c38e42081 .inf-success p a { color:#555555; font-size:14px; } .inf-main_41371be284c70c592049717c38e42081 h4{ font-size:18px; margin:0px 0px 13px 0px; } .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 label{ font-weight:bold; } .inf-main_41371be284c70c592049717c38e42081 .inf-input { margin-bottom:7px; } .inf-main_41371be284c70c592049717c38e42081 label { display:block;} .inf-main_41371be284c70c592049717c38e42081 input{ height:35px; color:#999999; border: 1px solid #E9E9E9; border:none; padding-left:7px; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error label, .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message{ color: #cc0033; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error input{ border: 1px solid #cc0033; } .inf-main_41371be284c70c592049717c38e42081 .inf-input input { width:100%;} .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message { display: block; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit { text-align:right;} .inf-main_41371be284c70c592049717c38e42081 .inf-submit input{ background-color:#777777; color:#ffffff; border:none; font-weight: normal; height:auto; padding:7px; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit input.disabled{ opacity: 0.4; } .inf-btn { color: rgb(85, 85, 85); border: medium none; font-weight: normal; height: auto; padding: 7px; display: inline-block; background-color: white; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.24); border-radius: 2px; line-height: 1em; } .inf-rgpd { margin:25px 0px 15px 0px; color:#555555; } </style><div class="inf-main_41371be284c70c592049717c38e42081"> <h4>Newsletter</h4> <span></span> <div class="inf-success" style="display:none"> <h4>Your registration has been successfully completed!</h4> <p> <a href="#" class="inf-btn">&laquo;</a> </p> </div> <div class="inf-content"> <div class="inf-input inf-input-text"> <input type="email" name="inf[1]" data-inf-meta="1" data-inf-error="" required="required" placeholder="Email *" > </div> <div class="inf-input inf-input-text"> <input type="text" name="inf[62029]" data-inf-meta="62029" data-inf-error="" placeholder="Nickname " > </div> <div class="inf-rgpd"></div> <div style=""> <label data-mcaptcha_url="https://captcha.infomaniak.com/widget/?sitekey=wKJaAigS1e48fWgqtjvg5w7rKA6QIwmy" for="mcaptcha__token" id="mcaptcha__token-label"> <input type="text" name="mcaptcha__token" id="mcaptcha__token"/> </label> <div id="mcaptcha__widget-container"></div> <script src="https://unpkg.com/@mcaptcha/vanilla-glue@0.1.0-rc2/dist/index.js"></script> </div> <div class="inf-submit"> <input type="submit" style="margin-top: 25px;" name="" value="Sign Up"> </div> </div> </div> </form>
+  <img class="logo" src="/images/logo.svg">
+  <div class="links">
+    <button type="button" onClick="location.href='https://www.instagram.com/tramundbass/'" formtarget="_blank">
+      Instagram
+    </button>
+  </div>
+
+  <div class="newsletter view">
+    <form method="post" action="https://newsletter.infomaniak.com/v3/api/1/newsletters/webforms/18147/submit" class="inf-form tnb-form">
+      <input type="email" name="email" style="display:none" />
+      <input type="hidden" name="key" value="eyJpdiI6Ik1qdEt3VGk1WXJQSzc1MkFKYkN3NExWT01WZ05TTTc4bkVRVEFiSHNaa3M9IiwibWFjIjoiZTE3OTZmMGFjYzJhMmEyODFhYmJmZmNhMThhMTExYjIxZGJkZWY3MDQ4ZDkxMTQ0MTFmMzk5YzJjYmI1OWM1MyIsInZhbHVlIjoibmxJY1h0eG9Gd3pBWGxDTG1ldzFSaDFUUTlua2NERWFJbHoxQUtPSW00bz0ifQ==">
+      <input type="hidden" name="webform_id" value="18147">
+      <style> .inf-main_41371be284c70c592049717c38e42081{ background-color:#FFFFFF; padding:16px 16px; margin:0px auto; } .inf-main_41371be284c70c592049717c38e42081 .inf-content { margin-top:12px;} .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 span, .inf-main_41371be284c70c592049717c38e42081 label, .inf-main_41371be284c70c592049717c38e42081 input, .inf-main_41371be284c70c592049717c38e42081 .inf-submit, .inf-main_41371be284c70c592049717c38e42081 .inf-success p a { color:#000000; font-size:14px; } .inf-main_41371be284c70c592049717c38e42081 h4{ font-size:1em; margin:0px 0px 12px 0px; } .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 label{ font-weight:bold; } .inf-main_41371be284c70c592049717c38e42081 .inf-input { margin-bottom:6px;} .inf-main_41371be284c70c592049717c38e42081 label { display:block;} .inf-main_41371be284c70c592049717c38e42081 input{ height:2em; color:#EEEEEE; border: 1px solid #EEEEEE; border:none; padding-left:16px; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error label, .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message{ color: #cc0033; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error input{ border: 1px solid #EEEEEE; } .inf-main_41371be284c70c592049717c38e42081 .inf-input input { width:100%;} .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message { display: block; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit { text-align:right;} .inf-main_41371be284c70c592049717c38e42081 .inf-submit input{ background-color:#000000; color:#FFFFFF; border:none; border-radius:0.48em; font-weight: normal; height:auto; padding:8px; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit input.disabled{ opacity: 0.4; } .inf-btn { color: rgb(85, 85, 85); border: medium none; font-weight: normal; height: auto; padding: 7px; display: inline-block; background-color: white; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.24); border-radius: 2px; line-height: 1em; } .inf-rgpd { margin:25px 0px 15px 0px; color:#555555; } </style>
+      <div class="inf-main_41371be284c70c592049717c38e42081"> 
+        <h4>Newsletter</h4> <span></span> <div class="inf-success" style="display:none"> <h4>Your registration has been successfully completed!</h4> <p> <a href="#" class="inf-btn">&laquo;</a> </p> </div> <div class="inf-content"> <div class="inf-input inf-input-text"> <input type="email" name="inf[1]" data-inf-meta="1" data-inf-error="" required="required" placeholder="Email [required]" > </div> <div class="inf-input tnb-input inf-input-text"> <input type="text" name="inf[62029]" data-inf-meta="62029" data-inf-error="" placeholder="Nickname " > </div> <div class="inf-rgpd"></div> <div style=""> <label data-mcaptcha_url="https://captcha.infomaniak.com/widget/?sitekey=wKJaAigS1e48fWgqtjvg5w7rKA6QIwmy" for="mcaptcha__token" id="mcaptcha__token-label"> <input type="text" name="mcaptcha__token" id="mcaptcha__token"/> </label> <div id="mcaptcha__widget-container"></div> <script src="https://unpkg.com/@mcaptcha/vanilla-glue@0.1.0-rc2/dist/index.js"></script> </div> <div class="inf-submit"> <input type="submit" style="margin-top: 25px;" name="" value="Sign Up"> </div> </div> </div> </form>
   </div>
 </div>
 
 <style lang="scss">
+  button {
+    background-color: white;
+    padding: 0.5em 0.8em;
+    color: black;
+    border: none;
+    border-radius: var(--border-radius-button);
+    cursor: pointer;
+    margin: 0.12em 0.12em 0.12em 0em;
+    transition: var(--transition);
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    text-align: center;
+  }
+
+  input{
+    background-color: #EEEEEE;
+    color: black !important;
+    border-radius: var(--border-radius-button);
+    height: 40px !important;
+    padding-top: 16px;
+    padding-bottom: 16px;
+  }
+
   .splash {
     z-index: 5000;
     display: inline;
@@ -20,18 +53,27 @@
     transform: translateY(-50%);
   }
 
+  .links{
+    width: fit-content;
+    margin-top: 2em;
+    margin-bottom: 2em;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
   .newsletter {
     width: 40vw;
     margin-left: auto;
     margin-right: auto;
   }
 
-  .logo {
-    width: auto;
-    height: 300px;
-    margin: auto;
+  .logo{
+    padding-top: 4em;
+    margin-left: auto;
+    width: 30vw;
+    height: auto;
+    margin-right: auto;
   }
-
   .supporters {
     display: flex;
     justify-content: space-between;
