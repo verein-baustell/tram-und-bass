f2b22237e3a551f933596865334710665589672a
Fabian Frey <50176941+fabidesign@users.noreply.github.com>
2024-09-07 11:40:55 +0200

    landing-page
    almost done

--- src/lib/LandingScreen.svelte
+++ src/lib/LandingScreen.svelte
@@ -1,11 +1,14 @@
 <div class="splash">
-  <img class="logo" src="/images/logo.svg">
-  <div class="links">
+  <div class="container">
+    <img class="logo" src="/images/TnB_logo.svg">
+    <div class="text">
+      <p>ab 15. November 2024</p>
+    </div>
+  <div class="links view">
     <button type="button" onClick="location.href='https://www.instagram.com/tramundbass/'" formtarget="_blank">
       Instagram
     </button>
   </div>
-
   <div class="newsletter view">
     <form method="post" action="https://newsletter.infomaniak.com/v3/api/1/newsletters/webforms/18147/submit" class="inf-form tnb-form">
       <input type="email" name="email" style="display:none" />
@@ -13,23 +16,90 @@
       <input type="hidden" name="webform_id" value="18147">
       <style> .inf-main_41371be284c70c592049717c38e42081{ background-color:#FFFFFF; padding:16px 16px; margin:0px auto; } .inf-main_41371be284c70c592049717c38e42081 .inf-content { margin-top:12px;} .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 span, .inf-main_41371be284c70c592049717c38e42081 label, .inf-main_41371be284c70c592049717c38e42081 input, .inf-main_41371be284c70c592049717c38e42081 .inf-submit, .inf-main_41371be284c70c592049717c38e42081 .inf-success p a { color:#000000; font-size:14px; } .inf-main_41371be284c70c592049717c38e42081 h4{ font-size:1em; margin:0px 0px 12px 0px; } .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 label{ font-weight:bold; } .inf-main_41371be284c70c592049717c38e42081 .inf-input { margin-bottom:6px;} .inf-main_41371be284c70c592049717c38e42081 label { display:block;} .inf-main_41371be284c70c592049717c38e42081 input{ height:2em; color:#EEEEEE; border: 1px solid #EEEEEE; border:none; padding-left:16px; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error label, .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message{ color: #cc0033; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error input{ border: 1px solid #EEEEEE; } .inf-main_41371be284c70c592049717c38e42081 .inf-input input { width:100%;} .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message { display: block; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit { text-align:right;} .inf-main_41371be284c70c592049717c38e42081 .inf-submit input{ background-color:#000000; color:#FFFFFF; border:none; border-radius:0.48em; font-weight: normal; height:auto; padding:8px; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit input.disabled{ opacity: 0.4; } .inf-btn { color: rgb(85, 85, 85); border: medium none; font-weight: normal; height: auto; padding: 7px; display: inline-block; background-color: white; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.24); border-radius: 2px; line-height: 1em; } .inf-rgpd { margin:25px 0px 15px 0px; color:#555555; } </style>
       <div class="inf-main_41371be284c70c592049717c38e42081"> 
-        <h4>Newsletter</h4> <span></span> <div class="inf-success" style="display:none"> <h4>Your registration has been successfully completed!</h4> <p> <a href="#" class="inf-btn">&laquo;</a> </p> </div> <div class="inf-content"> <div class="inf-input inf-input-text"> <input type="email" name="inf[1]" data-inf-meta="1" data-inf-error="" required="required" placeholder="Email [required]" > </div> <div class="inf-input tnb-input inf-input-text"> <input type="text" name="inf[62029]" data-inf-meta="62029" data-inf-error="" placeholder="Nickname " > </div> <div class="inf-rgpd"></div> <div style=""> <label data-mcaptcha_url="https://captcha.infomaniak.com/widget/?sitekey=wKJaAigS1e48fWgqtjvg5w7rKA6QIwmy" for="mcaptcha__token" id="mcaptcha__token-label"> <input type="text" name="mcaptcha__token" id="mcaptcha__token"/> </label> <div id="mcaptcha__widget-container"></div> <script src="https://unpkg.com/@mcaptcha/vanilla-glue@0.1.0-rc2/dist/index.js"></script> </div> <div class="inf-submit"> <input type="submit" style="margin-top: 25px;" name="" value="Sign Up"> </div> </div> </div> </form>
+        <h4>Newsletter</h4> <span></span> <div class="inf-success" style="display:none"> <h4>Your registration has been successfully completed!</h4> <p> <a href="#" class="inf-btn">&laquo;</a> </p> </div> <div class="inf-content"> <div class="inf-input inf-input-text"> <input type="email" name="inf[1]" data-inf-meta="1" data-inf-error="" required="required" placeholder="Email" > </div> <div class="inf-input tnb-input inf-input-text"> <input type="text" name="inf[62029]" data-inf-meta="62029" data-inf-error="" placeholder="Nickname " > </div> <div class="inf-rgpd"></div> <div style=""> <label data-mcaptcha_url="https://captcha.infomaniak.com/widget/?sitekey=wKJaAigS1e48fWgqtjvg5w7rKA6QIwmy" for="mcaptcha__token" id="mcaptcha__token-label"> <input type="text" name="mcaptcha__token" id="mcaptcha__token"/> </label> <div id="mcaptcha__widget-container"></div> <script src="https://unpkg.com/@mcaptcha/vanilla-glue@0.1.0-rc2/dist/index.js"></script> </div> <div class="inf-submit"> <input type="submit" style="margin-top: 25px;" name="" value="Sign Up"> </div> </div> </div> </form>
   </div>
+  <div class="support view">
+    <p class="center">Ganz viel Liebe</p>
+    <div class="s-grid">
+      <img class="supporter" src="/images/baustell.svg" alt="Stadt Supporter Logo" />
+      <img class="supporter" src="/images/rtfm.svg" alt="Migros Supporter Logo" />
+    </div>
+  </div>
+
+  <div class="support view">
+    <p class="center">Unterstützt von</p>
+    <div class="s-grid">
+      <img class="supporter" src="/images/Stadt.png" alt="Stadt Supporter Logo" />
+      <img class="supporter" src="/images/Migros.png" alt="Migros Supporter Logo" />
+      <img class="supporter" src="/images/ErnstGohner.png" alt="Ernst Göhner Supporter Logo" />
+      <img class="supporter" src="/images/Temperatio.png" alt="Temperatio Supporter Logo" />
+      <img class="supporter" src="/images/Kanton.png" alt="Kanton Supporter Logo" />
+      <img class="supporter" src="/images/VBZ.png" alt="VBZ Supporter Logo" />
+    </div>
+  </div>
+
+  </div>
+
+  
+  <img id="gif" src="/images/compressed_landing.gif">
 </div>
 
 <style lang="scss">
+  .break{
+    margin-top: 2em;
+  }
   button {
-    background-color: white;
+    background-color: black;
     padding: 0.5em 0.8em;
-    color: black;
+    color: white;
     border: none;
     border-radius: var(--border-radius-button);
     cursor: pointer;
-    margin: 0.12em 0.12em 0.12em 0em;
     transition: var(--transition);
     text-overflow: ellipsis;
     white-space: nowrap;
     text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+  }
+
+  button:hover{
+    background-color: #353535;
+  }
+
+  .support{
+    width: 100%;
+    padding-top: 1em;
+    margin-bottom: 0.8em;
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+
+  .center{
+    margin-left: auto;
+    margin-right: auto;
+    width: fit-content;
+  }
+
+  .text{
+    margin-top: 2em;
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .s-grid{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  .supporter{
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 2em;
+    margin-bottom: 2em;
   }
 
   input{
@@ -54,40 +124,57 @@
   }
 
   .links{
-    width: fit-content;
+    width: 100%;
     margin-top: 2em;
-    margin-bottom: 2em;
+    margin-bottom: 0.8em;
     margin-left: auto;
     margin-right: auto;
+  }
+
+  #gif{
+    position: fixed;
+    top: 0;
+    z-index: -1000;
+    min-height: 100vh;
+    scale: 1.1;
+    width: auto;
+    height: auto;
+    filter: saturate(1.5) blur(24px);
   }
 
   .newsletter {
+    width: 100%;
+    margin-bottom: 0.8em;
+  }
+
+  .container{
     width: 40vw;
     margin-left: auto;
     margin-right: auto;
+    height: 100vh;
+    overflow: scroll;
+    padding: var(--global-padding);
+  }
+  
+  .container::-webkit-scrollbar {
+  display: none;
   }
 
+  .container {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
   .logo{
     padding-top: 4em;
     margin-left: auto;
-    width: 30vw;
+    width: 100%;
     height: auto;
     margin-right: auto;
   }
-  .supporters {
-    display: flex;
-    justify-content: space-between;
-    margin-left: auto;
-    margin-right: auto;
-    width: calc(100vw - 1.6em);
-    padding: 1em;
-    align-items: center;
-    border-radius: var(--border-radius-view);
-    background-color: var(--background-color);
-  }
 
-  .supporter {
-    height: 30px;
-    width: auto;
+  @media only screen and (max-width: 600px) {
+  	.container {
+    	width: 100%;
   }
+}
 </style>
