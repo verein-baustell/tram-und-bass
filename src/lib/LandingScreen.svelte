9ea3f2f8d4e45883c9efbdf55d248d3baefc5829
Fabian Frey <50176941+fabidesign@users.noreply.github.com>
2024-09-17 10:48:59 +0200

    font

--- src/lib/LandingScreen.svelte
+++ src/lib/LandingScreen.svelte
@@ -1,29 +1,29 @@
 <div class="splash">
   <div class="container">
     <img class="logo" src="/images/TnB_logo.svg">
-    <div class="text">
+    <div class="text title release">
       <p>ab 15. November 2024</p>
     </div>
   <div class="links view">
-    <button type="button" onClick="location.href='https://www.instagram.com/tramundbass/'" formtarget="_blank">
-      Instagram
-    </button>
-    <button type="button" onClick="location.href='https://www.instagram.com/tramundbass/'" formtarget="_blank">
-      Tickets für Release Event am 15. November 2024
-    </button>
+    <p class="center title">Links</p>
+    <div class="abc">
+      <button class="center" type="button" onClick="location.href='https://www.instagram.com/tramundbass/'" formtarget="_blank">
+        Instagram
+      </button>
+    </div>
   </div>
   <div class="newsletter view">
-    <p class="center">Newsletter</p>
+    <p class="center title">Newsletter</p>
     <form method="post" action="https://newsletter.infomaniak.com/v3/api/1/newsletters/webforms/18147/submit" class="inf-form tnb-form">
       <input type="email" name="email" style="display:none" />
       <input type="hidden" name="key" value="eyJpdiI6Ik1qdEt3VGk1WXJQSzc1MkFKYkN3NExWT01WZ05TTTc4bkVRVEFiSHNaa3M9IiwibWFjIjoiZTE3OTZmMGFjYzJhMmEyODFhYmJmZmNhMThhMTExYjIxZGJkZWY3MDQ4ZDkxMTQ0MTFmMzk5YzJjYmI1OWM1MyIsInZhbHVlIjoibmxJY1h0eG9Gd3pBWGxDTG1ldzFSaDFUUTlua2NERWFJbHoxQUtPSW00bz0ifQ==">
       <input type="hidden" name="webform_id" value="18147">
-      <style> .inf-main_41371be284c70c592049717c38e42081{ background-color:#FFFFFF; padding:16px 16px; margin:0px auto; } .inf-main_41371be284c70c592049717c38e42081 .inf-content { margin-top:0px;} .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 span, .inf-main_41371be284c70c592049717c38e42081 label, .inf-main_41371be284c70c592049717c38e42081 input, .inf-main_41371be284c70c592049717c38e42081 .inf-submit, .inf-main_41371be284c70c592049717c38e42081 .inf-success p a { color:#000000; font-size:1em; } .inf-main_41371be284c70c592049717c38e42081 h4{ font-size:1em; margin:0px 0px 12px 0px; } .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 label{ font-weight:bold; } .inf-main_41371be284c70c592049717c38e42081 .inf-input { margin-bottom:6px;} .inf-main_41371be284c70c592049717c38e42081 label { display:block;} .inf-main_41371be284c70c592049717c38e42081 input{ height:2em; color:#EEEEEE; border: 1px solid #EEEEEE; border:none; padding-left:16px; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error label, .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message{ color: #cc0033; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error input{ border: 1px solid #EEEEEE; } .inf-main_41371be284c70c592049717c38e42081 .inf-input input { width:100%;} .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message { display: block; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit { text-align:center; min-width:100%;} .inf-main_41371be284c70c592049717c38e42081 .inf-submit input{ background-color:#000000; color:#FFFFFF!important; border:none; border-radius:0.48em; font-weight: normal; height:auto; padding:8px; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit input.disabled{ opacity: 0.4; } .inf-btn { color: rgb(85, 85, 85); border: medium none; font-weight: normal; height: auto; padding: 7px; display: inline-block; background-color: white; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.24); border-radius: 2px; line-height: 1em; } .inf-rgpd { margin:0px 0px 0px 0px; color:#555555; } </style>
+      <style> .inf-main_41371be284c70c592049717c38e42081{ background-color:#FFFFFF; padding:16px 16px; margin:0px auto; } .inf-main_41371be284c70c592049717c38e42081 .inf-content { margin-top:0px;} .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 span, .inf-main_41371be284c70c592049717c38e42081 label, .inf-main_41371be284c70c592049717c38e42081 input, .inf-main_41371be284c70c592049717c38e42081 .inf-submit, .inf-main_41371be284c70c592049717c38e42081 .inf-success p a { color:#000000; font-size:1em; } .inf-main_41371be284c70c592049717c38e42081 h4{ font-size:1em; margin:0px 0px 12px 0px; } .inf-main_41371be284c70c592049717c38e42081 h4, .inf-main_41371be284c70c592049717c38e42081 label{ font-weight:bold; } .inf-main_41371be284c70c592049717c38e42081 .inf-input { margin-bottom:6px;} .inf-main_41371be284c70c592049717c38e42081 label { display:block;} .inf-main_41371be284c70c592049717c38e42081 input{ height:2em; color:#EEEEEE; border: 1px solid #EEEEEE; border:none; padding-left:16px; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error label, .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message{ color: #cc0033; } .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error input{ border: 1px solid #EEEEEE; } .inf-main_41371be284c70c592049717c38e42081 .inf-input input { width:100%;} .inf-main_41371be284c70c592049717c38e42081 .inf-input.inf-error span.inf-message { display: block; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit { text-align:center; min-width:100%;} .inf-main_41371be284c70c592049717c38e42081 .inf-submit input{ background-color:#000000; color:#FFFFFF!important; cursor:pointer; border:none; border-radius:0.48em; font-weight: normal; height:auto; padding:8px; } .inf-main_41371be284c70c592049717c38e42081 .inf-submit input.disabled{ opacity: 0.4; } .inf-btn { color: rgb(85, 85, 85); border: medium none; font-weight: normal; height: auto; padding: 7px; display: inline-block; background-color: white; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.24); border-radius: 2px; line-height: 1em; } .inf-rgpd { margin:0px 0px 0px 0px; color:#555555; } </style>
       <div class="inf-main_41371be284c70c592049717c38e42081"> 
         <div class="inf-success" style="display:none"> <h4>Your registration has been successfully completed!</h4> <p> <a href="#" class="inf-btn">&laquo;</a> </p> </div> <div class="inf-content"> <div class="inf-input inf-input-text"> <input type="email" name="inf[1]" data-inf-meta="1" data-inf-error="" required="required" placeholder="Email" > </div> <div class="inf-input tnb-input inf-input-text"> <input type="text" name="inf[62029]" data-inf-meta="62029" data-inf-error="" placeholder="Nickname " > </div> <div class="inf-rgpd"></div> <div style=""> <label data-mcaptcha_url="https://captcha.infomaniak.com/widget/?sitekey=wKJaAigS1e48fWgqtjvg5w7rKA6QIwmy" for="mcaptcha__token" id="mcaptcha__token-label"> <input type="text" name="mcaptcha__token" id="mcaptcha__token"/> </label> <div id="mcaptcha__widget-container"></div> <script src="https://unpkg.com/@mcaptcha/vanilla-glue@0.1.0-rc2/dist/index.js"></script> </div> <div class="inf-submit"> <input type="submit" style="margin-top: 25px;" name="" value="Sign Up"> </div> </div> </div> </form>
   </div>
   <div class="support view">
-    <p class="center">Ganz viel Liebe</p>
+    <p class="center title">Ganz viel Liebe</p>
     <div class="s-grid">
       <img class="supporter" src="/images/baustell.svg" alt="Stadt Supporter Logo" />
       <img class="supporter" src="/images/rtfm.svg" alt="Migros Supporter Logo" />
@@ -31,7 +31,7 @@
   </div>
 
   <div class="support view">
-    <p class="center">Unterstützt von</p>
+    <p class="center title">Unterstützt von</p>
     <div class="s-grid">
       <img class="supporter" src="/images/Stadt.png" alt="Stadt Supporter Logo" />
       <img class="supporter" src="/images/Migros.png" alt="Migros Supporter Logo" />
@@ -60,13 +60,24 @@
     border-radius: var(--border-radius-button);
     cursor: pointer;
     transition: var(--transition);
-    text-align: center;
-    margin-left: auto;
-    margin-right: auto;
   }
 
   button:hover{
     background-color: #353535;
+  }
+
+  .release{
+    color: black;
+    background-color: white;
+    border-radius: 2em;
+    padding-top: 0.2em;
+    padding-left: 0.6em;
+    padding-right: 0.6em;
+  }
+
+  .abc{
+    width: 100%;
+    display: flex;
   }
 
   .support{
@@ -75,6 +86,12 @@
     margin-bottom: 0.8em;
     padding-left: 1em;
     padding-right: 1em;
+  }
+
+  .title{
+    font-family: Rene;
+    font-size: 1.2em;
+    margin-bottom: 0.6em;
   }
 
   .center{
